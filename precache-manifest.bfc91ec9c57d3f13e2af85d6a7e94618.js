e20970e924e7cf6821b7be68f140b2747b111f6b
Hadi Ahmed <969hadiahmed@gmail.com>
2023-05-16 10:51:02 +0530

    Updates

---
+++ precache-manifest.bfc91ec9c57d3f13e2af85d6a7e94618.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3ef9f38078bae76366e",
+    "url": "/robo-friend/static/css/main.cb283f9d.chunk.css"
+  },
+  {
+    "revision": "d3ef9f38078bae76366e",
+    "url": "/robo-friend/static/js/main.d3ef9f38.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/robo-friend/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "7f1d01298ba374c9a3b1",
+    "url": "/robo-friend/static/css/2.5a59c7e9.chunk.css"
+  },
+  {
+    "revision": "7f1d01298ba374c9a3b1",
+    "url": "/robo-friend/static/js/2.7f1d0129.chunk.js"
+  },
+  {
+    "revision": "86e4d47bd572a1d8d496",
+    "url": "/robo-friend/static/js/runtime~main.86e4d47b.js"
+  },
+  {
+    "revision": "ea3c7fc31f99f4a20b63aa61f0f6be86",
+    "url": "/robo-friend/index.html"
+  }
+];
